da7e9dd80bbac7cf0a866982067de5d6039886ca
weipai_lz <weipai_lz@qq.com>
2020-03-16 18:11:52 +0800

    2020-3-16 18:11:46

--- src/common/lib/Server/ServerRoute.php
+++ src/common/lib/Server/ServerRoute.php
@@ -9,6 +9,33 @@ class ServerRoute {
 	use NameBase;
 	use ResultBase;
 	
+	// key
+	const KEY_HOST = 'host';
+	const KEY_TYPE = 'type';
+	
+	// type
+	const TYPE_EVENT = 'event';
+	
+	/**
+	 * 所有服务器配置
+	 * @var array $_serverConfig
+	 */
+	protected $_config = [];
+	
+	/**
+	 * 服务器名称
+	 *  通过名称查找对应服务器
+	 * @var string $_name
+	 */
+	protected $_name = 'main';
+	
+	/**
+	 * 服务类型
+	 *  例如：事件event
+	 *
+	 * @var string $_type
+	 */
+	protected $_type = 'event';
 	
 	/**
 	 * Local constructor.
@@ -37,9 +64,56 @@ class ServerRoute {
 		$this->name_info['intro'] = '服务器配置';
 	}
 	
+	/**
+	 * 服务器名称
+	 *
+	 * @param null $name
+	 * @return $this|string
+	 */
+	public function name($name = null) {
+		if ($name === null) {
+			return $this->_name;
+		} else {
+			$this->_name = $name;
+		}
+		
+		return $this;
+	}
+	
+	/**
+	 * 服务类型
+	 *  例如：event
+	 *
+	 * @param null $type
+	 * @return $this|string
+	 */
+	public function type($type = null) {
+		if ($type === null) {
+			return $this->_type;
+		} else {
+			$this->_type = $type;
+		}
+		
+		return $this;
+	}
+	
 	/**************************************************
 	 * getter setter
 	 **************************************************/
+	
+	/**
+	 * @return array
+	 */
+	public function getConfig(): array {
+		return $this->_config;
+	}
+	
+	/**
+	 * @param array $config
+	 */
+	public function _setConfig(array $config) {
+		$this->_config = $config;
+	}
 	
 	
 	
--- src/config/server_config.php
+++ src/config/server_config.php
@@ -3,12 +3,21 @@
 use uujia\framework\base\common\consts\ServerConst;
 
 return [
-	'server_event' => [
+	'server' => [
+		// 服务器名称name
 		'main' => [
-			'host'  => 'localhost',                     // 域名地址
-			'async' => false,                           // 是否异步
-			'type'  => ServerConst::TYPE_LOCAL_NORMAL,  // 请求类型
-			'url'   => '',                              // 接口地址（如果需要远程POST请求 对端接口地址）
+			// 域名地址
+			'host'  => 'localhost',
+			
+			// 服务类型type
+			'type' => [
+				'event' => [
+					'async' => false,                           // 是否异步
+					'type'  => ServerConst::TYPE_LOCAL_NORMAL,  // 请求类型
+					'url'   => '',                              // 接口地址（如果需要远程POST请求 对端接口地址）
+				],
+			],
+			
 		],
 	],
 
